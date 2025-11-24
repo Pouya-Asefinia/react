675d4b4adfd08e03b7f389923b85ae1416e1d365
Pouya <p.asefinia83@gmail.com>
2025-11-24 13:15:13 +0330

    Implement github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
+var ghpages = require('gh-pages');
+ghpages.publish('dist', {
+  branch: 'main',
+  repo: 'https://github.com/Pouya-Asefinia/Shopping_cart.git'
+}, callback);
 
 import './index.css'
 
